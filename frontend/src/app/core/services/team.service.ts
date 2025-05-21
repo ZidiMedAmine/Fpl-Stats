58cfa2b1c40e046807a43551eceea93810b2ecdc
ZidiMedAmine <medzidiamine@gmail.com>
2025-05-21 04:52:02 +0100

    Add WebConfig to configure CrossOrigin. Updated environment files after hosting on Render.com.

--- frontend/src/app/core/services/team.service.ts
+++ frontend/src/app/core/services/team.service.ts
@@ -2,14 +2,15 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {UserInfo} from "../models/UserInfo.model";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
-  protected apiUrl = 'http://localhost:8080/api/fpl';
+  protected apiUrl = `${environment.apiBaseUrl}/fpl`;
 
-  constructor(protected http: HttpClient) { }
+  constructor(protected http: HttpClient) {}
 
   getTeamPlayers(teamId: number): Observable<UserInfo> {
     return this.http.get<UserInfo>(`${this.apiUrl}/team-players/${teamId}`);
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'https://fpl-stats-6zlu.onrender.com/api'
 };
