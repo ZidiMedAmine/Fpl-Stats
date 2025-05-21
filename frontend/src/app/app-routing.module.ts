498c42a059237e63a244dca6cf2cda5b63685e67
ZidiMedAmine <medzidiamine@gmail.com>
2025-05-21 15:23:42 +0100

    Minor updates.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'team-players/:id', loadChildren: () => import('./modules/team-players/team-players.module').then(m => m.TeamPlayersModule) },
-  { path: '', redirectTo: 'team-players', pathMatch: 'full' }];
+const routes: Routes = [{ path: 'team-players/:id', loadChildren: () => import('./modules/team-players/team-players.module').then(m => m.TeamPlayersModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
