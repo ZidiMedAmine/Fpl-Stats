e641e8c3823bfef052f22c70dcba1789ee53a1c9
ZidiMedAmine <medzidiamine@gmail.com>
2025-05-21 16:23:31 +0100

    Fix: Setup for Lazy Loaded Route with :id

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'team-players/:id', loadChildren: () => import('./modules/team-players/team-players.module').then(m => m.TeamPlayersModule) }];
+const routes: Routes = [
+  { path: 'team-players',
+    loadChildren: () =>
+      import('./modules/team-players/team-players.module').then(m => m.TeamPlayersModule)
+  },
+  { path: '', redirectTo: 'team-players/3514734', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
